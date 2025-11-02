9477bc265f0999a3ad2e7d06d662a7d21e6ca692
aleksey antonov <antonovaaa174@gmail.com>
2025-11-02 14:36:07 +0500

    Add .gitignore and renamed files to homework to lesson 3

--- test_google.py
+++ test_google.py
@@ -1,12 +1,13 @@
 import pytest
 from selenium import webdriver
 
+
 @pytest.mark.google
 def test_google():
     driver = webdriver.Chrome()
-    url = 'https://www.google.com/'
+    url = "https://www.google.com/"
 
     driver.get(url)
 
-    assert driver.title == 'Google'
+    assert driver.title == "Google"
     assert driver.current_url == url
